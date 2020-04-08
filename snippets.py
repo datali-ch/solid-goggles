3143684bf3b919715366d5ed61bb9ba027af17c3
magdalena <magdalena@nxgen.info>
2020-04-08 18:02:31 +0200

    time series forecasting

--- snippets.py
+++ snippets.py
@@ -178,3 +178,16 @@ def visualize_convolutions(model: tf.keras.models.Model, image: str) -> None:
 			plt.title(layer_name)
 			plt.grid(False)
 			plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+
+def preprocess_timeseries(series: List[float], window_size: int, batch_size: int, shuffle_buffer: int) -> tf.data.Dataset:
+
+    series = tf.expand_dims(series, axis=-1)
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    ds = ds.shuffle(buffer_size=shuffle_buffer)
+    ds = ds.map(lambda w: (w[:-1], w[1:]))
+    ds = ds.batch(batch_size).prefetch(1)
+
+    return ds
